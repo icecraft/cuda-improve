b8ede2f397f029b7cbe988a33fec085e326767c6
rui <xurui@supremind.com>
2021-05-06 16:54:54 +0800

    1.fix compile error

---
+++ mnist/layers.h
@@ -0,0 +1,19 @@
+#ifndef MY_LAYERS_CONFIG_H_
+#define MY_LAYERS_CONFIG_H_
+#include <cuda_runtime.h>
+
+const int N = 59136;
+const int D = 784;
+const int H = 1000;
+const int C = 10;
+
+__device__ float MNIST_data[N][D];
+__device__ int MNIST_label[N];
+
+__device__ float fc1[H][D];
+__device__ float b1[H];
+
+
+__device__ float fc2[C][H];
+__device__ float b2[C];
+#endif 
--- mnist/mnist.cu
+++ mnist/mnist.cu
@@ -1,9 +1,10 @@
 
-
+# include <cuda_runtime.h>
+# include "layers.h"
 
 int main(int argc, char **argv) {
   
   dim3 block(128, 1);
   dim3 grid(14, 1);
-  train_mnist<<< grid, block >>><14>();
+  train_mnist<33><<< grid, block >>>();
 }
