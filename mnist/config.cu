4b712f161bcdefe93e04c7fedaeb9c3cbfe7ef66
rui <xurui@supremind.com>
2021-05-07 11:19:33 +0800

    1.补提交 config.cu

---
+++ mnist/config.cu
@@ -0,0 +1,26 @@
+# include <cuda_runtime.h>
+# include "config.h"
+
+__device__ float MNIST_data[NN][D];
+__device__ int MNIST_label[NN];
+
+__device__ float fc1[H][D];
+__device__ float b1[H];
+__device__ float fc2[C][H];
+__device__ float b2[C];
+
+
+float h_fc1[H][D];
+float h_b1[H];
+float h_fc2[C][H];
+float h_b2[C];
+
+__device__ float d_g_d_fc1_w[H][D]; // device(d) global(g) deviration(d)
+__device__ float d_g_d_fc1_b[H];
+__device__ float d_g_d_fc2_w[C][H];
+__device__ float d_g_d_fc2_b[C];
+
+float h_g_d_fc1_w[H][D];
+float h_g_d_fc1_b[H];
+float h_g_d_fc2_w[C][H];
+float h_g_d_fc2_b[C];
